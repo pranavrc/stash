341dc0cfda53c0274dd3c661009a5092362246c6
Pranav Ravichandran <me@onloop.net>
2013-12-28 18:31:13 +0530

    Doccing up a bit

--- server.lisp
+++ server.lisp
@@ -4,6 +4,7 @@
 (defvar *server* nil)
 
 (defun initialize (port &optional (host usocket:*wildcard-host*))
+  "Initialize the server at the host's port"
   (let ((socket (usocket:socket-listen host port :reuse-address t)))
     (setf *server*
 	  (bt:make-thread #'(lambda ()
@@ -13,6 +14,7 @@
 			  :name (format nil "Server at ~a port" port)))))
 
 (defun serve (socket)
+  "Create a socket stream and open it"
   (loop
      (usocket:wait-for-input socket :timeout 5)
      (let ((stream (usocket:socket-stream (usocket:socket-accept socket))))
@@ -22,6 +24,7 @@
 		       :name (format nil "Request handler for ~s" stream)))))
 
 (defun handle-request (stream)
+  "Handle stream data"
   (let ((line (read-line stream)))
     (format stream "~a" line))
   (terpri stream)
@@ -29,11 +32,13 @@
 
 #+bordeaux-threads
 (defun terminate ()
+  "Terminate the server thread"
   (let ((server (shiftf *server* nil)))
     (when server
       (bt:destroy-thread server))))
 
 (defun client (port command &optional (host usocket:*wildcard-host*))
+  "Client function to write to the server stream"
   (let* ((socket (usocket:socket-connect host port))
 	 (stream (usocket:socket-stream socket)))
     (write-line command stream)
