4ca260a8acd299b2a410581ac0ca8fb44da1e1c4
Pranav Ravichandran <me@onloop.net>
2013-12-28 18:31:23 +0530

    String split function

--- server.lisp
+++ server.lisp
@@ -47,3 +47,10 @@
       (close stream)
       (usocket:socket-close socket)
       result)))
+
+(defun string-split (string delim)
+  "Splits a string into substrings around the delimiter."
+  (loop for x = 0 then (1+ y)
+     as y = (position delim string :start x)
+     collect (subseq string x y)
+     while y))
